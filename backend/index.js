6607ee25b65a0dabb126cd39f2747eeaff1a14a8
Bill Z. Qin <bzqin88@gmail.com>
2020-02-15 13:20:19 -0500

    added auth, listings for companies/users

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ const jwt = require('jsonwebtoken');
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const request = require("request");
+const uuidv1 = require("uuid/v1");
 
 const app = express();
 
@@ -25,9 +26,24 @@ knex.schema.hasTable('users').then(function(exists){
     table.increments('id');
     table.string('email');
     table.string('password');
+    table.json('profile').nullable();
+    table.json('companylist');
   })
   .then(function(){
     console.log("Successfully created 'users' table");
+  });
+});
+
+knex.schema.hasTable('companies').then(function(exists){
+  if(!exists) knex.schema.createTable('companies', function(table){
+    table.increments('id');
+    table.string('company');
+    table.string('password');
+    table.string('companykey');
+    table.json('userlist');
+  })
+  .then(function(){
+    console.log("Successfully created 'companies' table");
   });
 });
 
@@ -53,53 +69,153 @@ var verifyToken = function(req, res, next){
 };
 
 //User routing
-app.post("/signup", function(req, res) {
+app.post("/usersignup", function(req, res) {
   //create user, assign token, and send them to app
   //with help from link in verifyToken
-  knex("users").select("*").where("email", req.query.email).then((response) => {
+  knex("users").select("*").where("email", req.body.email).then((response) => {
     if (!response.length){
-      let hash = bcrypt.hashSync(req.query['password'],10);
+      let hash = bcrypt.hashSync(req.body.password,10);
       knex("users").insert({
-        email: req.query['email'],
-        password: hash
+        email: req.body.email,
+        password: hash,
+        profile: null,
+        companylist: '{list:[]}'
+        /**
+        A "companylist" looks like:
+        {
+          list:
+          [
+            {companyid: 2, applied_at: Date()},
+            {companyid: 4, applied_at: Date()}
+          ]
+        }
+        **/
       }).then(function() {
-        var token = jwt.sign({ id: req.query.email }, process.env.JWT_SECRET);
+        var token = jwt.sign({ id: req.body.email }, process.env.JWT_SECRET);
         res.status(200).send({ auth: true, token: token });
       })
     }
   })
-  res.send(req.query);
+  res.send(req.body);
 })
 
-app.post('/login', function(req, res, next) {
+app.post("/companysignup", function(req, res) {
+  //create user, assign token, and send them to app
+  //with help from link in verifyToken
+  knex("companies").select("*").where("company", req.body.company).then((response) => {
+    if (!response.length){
+      let hash = bcrypt.hashSync(req.body.password,10);
+      knex("users").insert({
+        company: req.body.company,
+        password: hash,
+        companykey: uuidv1(),
+        userlist: '{list:[]}'
+        /**
+        A "userlist" looks like:
+        {
+          list:
+          [
+            {userid: 2, applied_at: Date()},
+            {userid: 4, applied_at: Date()}
+          ]
+        }
+        **/
+      }).then(function() {
+        var token = jwt.sign({ id: req.body.company }, process.env.JWT_SECRET);
+        res.status(200).send({ auth: true, token: token });
+      })
+    }
+  })
+  res.send(req.body);
+})
+
+app.post('/userlogin', function(req, res, next) {
   // find user from table, assign token, then send them to app
   // with help from link in verifyToken
-  knex("users").select("*").where("email", req.query.email)
+  console.log(req.body);
+  knex("users").select("*").where("email", req.body.email)
   .then(function(response){
-    if(bcrypt.compareSync(req.query.password, response[0].password)){
-      var token = jwt.sign({ id: req.query.email }, process.env.JWT_SECRET);
+    if(bcrypt.compareSync(req.body.password, response[0].password)){
+      var token = jwt.sign({ id: req.body.email }, process.env.JWT_SECRET);
       res.status(200).send({ auth: true, token: token });
     }
     else return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
   });
 });
 
+app.post('/companylogin', function(req, res, next) {
+  // find user from table, assign token, then send them to app
+  // with help from link in verifyToken
+  console.log(req.body);
+  knex("companies").select("*").where("company", req.body.company)
+  .then(function(response){
+    if(bcrypt.compareSync(req.body.password, response[0].password)){
+      var token = jwt.sign({ id: req.body.company }, process.env.JWT_SECRET);
+      res.status(200).send({ auth: true, token: token });
+    }
+    else return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+  });
+});
+
+app.post('/userapply/s1', function(req, res, next)
+{
+  //Given a certain companykey, userapply will take user information and add
+  //its id to that company's list, should authentication succeed.
+  //Stage 1: logging in to provide to companies.
+  knex("companies").select("*").where("companykey",req.params['key'])
+  .then(function(response){
+    res.status(200).send({companyauth: true, key: req.params['key']});
+  })
+});
+
+app.post('/userapply/s2', function(req, res, next)
+{
+  //Given a certain companykey, userapply will take user information and add
+  //its id to that company's list, should authentication succeed.
+  //Stage 2: checking authentication: if it succeeds, add it to the company's database
+  knex("companies").select("*").where("companykey",req.params['key'])
+  .then(function(companyres){
+    knex.("users").select("*").where("email",req.body.email)
+    .then(function(userres){
+      if(bcrypt.compareSync(req.body.password, userres[0].password)){
+        var token = jwt.sign({ id: req.body.company }, process.env.JWT_SECRET);
+        userlist = userres[0].companylist.toJSON().list;
+        console.log(userlist);
+        companylist = companyres[0].userlist.toJSON().list;
+        console.log(companylist);
+        userlist.push(JSON.stringify({companyid: companyres[0].id, applied_at: new Date()}));
+        companylist.push(JSON.stringify({userid: userres[0].id, applied_at: new Date()}));
+        knex.("companies").select("*").where("companykey",req.params['key']).update({userlist: JSON.stringify({list: userlist})});
+        knex.("users").select("*").where("email",req.body.email).update({companylist: JSON.stringify({list: companylist})});
+        res.status(200).send({ auth: true, token: token });
+      }else{
+        return res.status(500).send({auth: false, message: 'Failed to authenticate token.'});
+      }
+    });
+  })
+});
 //gets details of current user
 app.get("/user/current", verifyToken, function(req, res, next) {
   res.status(200).send(req.user)
-})
+});
 
 app.get("/users/", function(req, res) {
   knex.from("users").select("*").then(function(results){
     res.json(results);
   })
-})
+});
+
+app.get("/companies/", function(req, res) {
+  knex.from("companies").select("*").then(function(results){
+    res.json(results);
+  })
+});
 
 app.get("/users/:id", function(req, res) {
   knex.from("users").where("id", req.params.id).then(function(results){
     res.json(results);
   })
-})
+});
 
 
 app.listen(process.env.PORT || 3000, function(){
